ff17708debf209c14e689023054915610ac07379
DongGunYoon <donggun961108@gmail.com>
2024-08-01 11:04:35 +0900

    feat: 콘서트 관련 조회 기능 캐싱 적용 및 테스트 추가
    - 콘서트 관련 조회 기능 캐싱 적용
      - 콘서트 목록 조회 API에 캐시 추가
      - 예약 가능한 스케줄 목록 조회 API에 캐시 추가
    
    - 콘서트 예약 가능한 스케쥴 목록 조회 캐시 테스트 추가
      - 100번이 요청이 들어와도 한번의 DB 요청이 들어가는지 테스트

--- src/application/concert/use-case/scan-bookable-schedules.use-case.impl.ts
+++ src/application/concert/use-case/scan-bookable-schedules.use-case.impl.ts
@@ -1,13 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Cache } from '@nestjs/cache-manager';
+import { Inject, Injectable } from '@nestjs/common';
+import { Nullable } from 'src/common/type/native';
 import { ScanBookableSchedulesUseCase } from 'src/domain/concert/interface/use-case/scan-bookable-schedules.use-case';
 import { ConcertSchedule } from 'src/domain/concert/model/concert-schedule.domain';
 import { ConcertService } from 'src/domain/concert/service/concert.service';
 
 @Injectable()
 export class ScanBookableSchedulesUseCaseImpl implements ScanBookableSchedulesUseCase {
-  constructor(private readonly concertService: ConcertService) {}
+  constructor(
+    private readonly concertService: ConcertService,
+    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
+  ) {}
 
   async execute(concertId: number): Promise<ConcertSchedule[]> {
-    return await this.concertService.scanBookableSchedueles(concertId);
+    let bookableSchedules = (await this.cacheManager.get(this.getCacheKey(concertId))) as Nullable<ConcertSchedule[]>;
+
+    if (!bookableSchedules) {
+      bookableSchedules = await this.concertService.scanBookableSchedueles(concertId);
+      await this.cacheManager.set(this.getCacheKey(concertId), bookableSchedules);
+    }
+
+    return bookableSchedules;
+  }
+
+  private getCacheKey(concertId: number): string {
+    return `concerts:${concertId}:bookable_schedules`;
   }
 }
--- src/application/concert/use-case/scan-concerts.use-case.impl.ts
+++ src/application/concert/use-case/scan-concerts.use-case.impl.ts
@@ -1,13 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Cache } from '@nestjs/cache-manager';
+import { Inject, Injectable } from '@nestjs/common';
+import { Nullable } from 'src/common/type/native';
 import { ScanConcertsUseCase } from 'src/domain/concert/interface/use-case/scan-concerts.use-case';
 import { Concert } from 'src/domain/concert/model/concert.domain';
 import { ConcertService } from 'src/domain/concert/service/concert.service';
 
 @Injectable()
 export class ScanConcertsUseCaseImpl implements ScanConcertsUseCase {
-  constructor(private readonly concertService: ConcertService) {}
+  constructor(
+    private readonly concertService: ConcertService,
+    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
+  ) {}
 
   async execute(): Promise<Concert[]> {
-    return await this.concertService.scanConcerts();
+    let concerts = (await this.cacheManager.get(this.getCacheKey())) as Nullable<Concert[]>;
+
+    if (!concerts) {
+      concerts = await this.concertService.scanConcerts();
+      await this.cacheManager.set(this.getCacheKey(), concerts);
+    }
+
+    return concerts;
+  }
+
+  private getCacheKey(): string {
+    return 'concerts';
   }
 }
