a97beb7b545ca7366ce22efee1f4e4e53c3217fd
DongGunYoon <donggun961108@gmail.com>
2024-08-16 01:10:49 +0900

    feat: 카프카 도커 설정 및 마이크로 서비스 적용
    - Dokcer-compose에 카프카 설정 추가
    - main.ts에 microservice를 통한 카프카 consumer group 추가

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,39 @@ import { AppModule } from './module/app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { readFileSync } from 'fs';
 import * as path from 'path';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'offset-consumer',
+      },
+    },
+  });
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'data-platform-consumer',
+      },
+    },
+  });
+
   const swaagerConfig = readFileSync(path.join(__dirname, '../../swagger.json'), 'utf8');
   SwaggerModule.setup('api-docs', app, JSON.parse(swaagerConfig));
 
+  await app.startAllMicroservices();
   await app.listen(3000);
 }
 
